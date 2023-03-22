32d16a9edb5a250935cea9d64a11e2b10fd933b2
prettywebthings <>
2023-03-22 12:39:09 +0000

    Update from https://github.com/prettywebthings/website/commit/1ea6c0fd3a6c174489b566fe4b0a24e286bc2249

---
+++ assets/ActivityLog-d46f706e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-935e362f.js";const t={};function r(n,l){return c(),o("div",null,"hello world")}const _=e(t,[["render",r]]);export{_ as default};
--- assets/ActivityLog-f4dcb9e4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-f53b8beb.js";const t={};function r(n,l){return c(),o("div",null,"hello world")}const _=e(t,[["render",r]]);export{_ as default};
