0139fa655aea16338bf9b34f173faab8c8eb2baf
prettywebthings <>
2023-04-11 02:36:04 +0000

    Update from https://github.com/prettywebthings/website/commit/cb53b2d797abeac04061daf4c12f830f11d60530

---
+++ assets/ActivityLog-99ae7cfd.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-9597c5f6.js";const o={},s={class:"text-blue"};function r(n,_){return t(),c("div",s,"hello world")}const l=e(o,[["render",r]]);export{l as default};
